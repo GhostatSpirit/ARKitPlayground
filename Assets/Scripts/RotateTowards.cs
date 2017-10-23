4557280d5c349a28383e7b2e30a66e0618a4e595
GhostatSpirit <lykavin@hotmail.com>
2017-10-23 12:51:15 +0800

    added zombie starter project

---
+++ Assets/Scripts/RotateTowards.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTowards : MonoBehaviour {
+
+	public Transform target;
+
+	public float rotateSpeed = 20f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 toTarget = target.position - transform.position;
+		Quaternion qTo = Quaternion.LookRotation (toTarget);
+		transform.rotation = Quaternion.RotateTowards (transform.rotation, qTo, rotateSpeed * Time.deltaTime);
+	}
+}
