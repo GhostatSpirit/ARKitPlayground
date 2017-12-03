515e16d8537769a0b01ae63fa60e6a52bd67d6b6
Yang Liu <lykavin@hotmail.com>
2017-12-03 20:10:24 +0800

    some experiment code, may be deleted in the future

---
+++ Assets/Scripts/Experiment/RaycastExperiment.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastExperiment : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+		if (Input.GetKeyDown (KeyCode.RightShift)) {
+			Collider coll = GetComponent<Collider> ();
+			coll.enabled = false;
+
+			if (Physics.Raycast (transform.position, Vector3.up, 0.1f)) {
+				Debug.Log ("collider still enabled!!");
+			} else {
+				Debug.Log ("collider disabled!!");
+			}
+		}
+	}
+}
