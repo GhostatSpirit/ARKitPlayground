3aca987b71b37b362aa12f4167fd6daf5b48f256
Yang Liu <lykavin@hotmail.com>
2017-12-07 20:17:54 +0800

    bullet material updated, added resolution scaler for retina iPad

---
+++ Assets/Scripts/Utility/ResolutionScaler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionScaler : MonoBehaviour {
+
+	void OnAwake(){
+		var screenPixels = Screen.width * Screen.height;
+		if(screenPixels > 2074000){
+			Screen.SetResolution (Screen.width / 2, Screen.height / 2, true, 0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
