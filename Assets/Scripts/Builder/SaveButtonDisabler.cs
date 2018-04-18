ad5ca2a8b12b975d302140066fe2a3b54ac018bd
Yang Liu <lykavin@hotmail.com>
2018-04-18 19:01:45 +0800

    more robust saver + CubeBuilder now align to cubePos instead of grid

---
+++ Assets/Scripts/Builder/SaveButtonDisabler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveButtonDisabler : MonoBehaviour {
+
+    public CubeBuilder cubeBuiler;
+
+    Button button;
+    // Use this for initialization
+    void Start () {
+        button = GetComponent<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(cubeBuiler.fallingCount > 0)
+        {
+            button.interactable = false;
+        } else
+        {
+            button.interactable = true;
+        }
+	}
+}
