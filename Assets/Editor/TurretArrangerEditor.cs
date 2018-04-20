00e90cbba836db01bf2fd4a8a9173144f785bf0f
Yang Liu <lykavin@hotmail.com>
2018-04-20 09:41:18 +0800

    fixed tracing projectile bug

---
+++ Assets/Editor/TurretArrangerEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TurretArranger))]
+public class TurretArrangerEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        TurretArranger arranger = (TurretArranger)target;
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Rearrange"))
+        {
+            arranger.rearrangeAllTurret();
+        }
+
+    }
+}
